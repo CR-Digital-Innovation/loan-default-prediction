9aa675a52a3d2aa30cf411a5430aa96f02210f9f
SaiShiva G <saishiva.gudla@criticalriver.com>
2023-06-14 07:01:57 +0000

    commiting testfile

--- testfile.py
+++ testfile.py
@@ -1,4 +1,5 @@
-from src.utils.s3_Functions import *
+#from src import utils.S3Utils
+from src.utils.s3_Functions import S3Utils
 from src.utils.load_EnvVars import (
     AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY,
@@ -8,7 +9,10 @@ from src.utils.load_EnvVars import (
     MODEL_NAME,
     MODEL_VERSION,
 )
-#import src
+
+
+import sys
+sys.path.append('src')
 
 print(AWS_ACCESS_KEY_ID, AWS_S3_BUCKET)
 
@@ -18,4 +22,4 @@ s3_utils = S3Utils(
 
 print(s3_utils.check_file_exist(AWS_S3_DATA_DIRECTORY_MODELS, f"{MODEL_NAME}_{MODEL_VERSION}.pkl"))
 
-model = s3_utils.load_pickle(AWS_S3_DATA_DIRECTORY_MODELS, f"{MODEL_NAME}_{MODEL_VERSION}.pkl")
+model = s3_utils.load_pickle(AWS_S3_DATA_DIRECTORY_MODELS, f"{MODEL_NAME}_{MODEL_VERSION}.pkl", compressed=False)
